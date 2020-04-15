23a00547cf4ccb9be5d6ad95aedc96f88678953c
YangJiaHui007 <819582890@qq.com>
2020-04-15 18:22:35 +0800

    edit

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -20,13 +20,18 @@ EXPOSE=${EXPOSE}
 jenkins_jar_path='/var/jenkins_home/workspace'
 # 构建镜像路径
 projects_path='/var/jenkins_home/docker_images/'
-
+Mounts=${Mounts}
+data=`date '+%Y%m%d%H%M%S'`
 # 停止删除容器
+{
 sudo docker stop ${app_name}
 echo 'stop container '${app_name}' success!'
 sudo docker rm ${app_name}
 echo 'delete container '${app_name}' success!'
 
+} || {
+echo 'stop container '${app_name}' Error!!!!'
+}
 # 复制jar包到指定目录
 sudo mkdir -p ${projects_path}${app_name}
 sudo cp ${jenkins_jar_path}/${app_file_name}/target/${app_name}-${maven_version}.jar  ${projects_path}${app_name}/
@@ -38,13 +43,18 @@ sudo docker login --username=admin --password=harbor http://${harbor_registry}
 sudo docker build -t ${image_prefix}/${app_name}:${maven_version} -f ${projects_path}${app_name}/Dockerfile ${projects_path}${app_name}/.
 
 sudo docker tag ${image_prefix}/${app_name}:${maven_version} ${harbor_registry}/${image_prefix}/${app_name}:${version}
-
 sudo docker push ${harbor_registry}/${image_prefix}/${app_name}:${version}
+
+sudo docker tag ${image_prefix}/${app_name}:${maven_version} ${harbor_registry}/${image_prefix}/${app_name}:${data}
+sudo docker push ${harbor_registry}/${image_prefix}/${app_name}:${data}
 
 sudo docker rmi `sudo docker images|grep none | awk '{print $3}'`
 sudo docker rmi ${image_prefix}/${app_name}:${maven_version}
 # 运行容器
-sudo docker run -p ${EXPOSE}:${INIT_EXPOSE} --name ${app_name} -v /etc/localtime:/etc/localtime -v ${projects_path}${app_name}/logs:/var/logs -dit ${harbor_registry}/${image_prefix}/${app_name}:${version}
+echo 'stop run shell:  'docker run -p ${EXPOSE}:${INIT_EXPOSE} --name ${app_name} ${Mounts}  -v /etc/localtime:/etc/localtime -v ${projects_path}${app_name}/logs:/var/logs -dit ${harbor_registry}/${image_prefix}/${app_name}:${version}''
+
+sudo docker run -p ${EXPOSE}:${INIT_EXPOSE} --name ${app_name}  ${Mounts}  -v /etc/localtime:/etc/localtime -v ${projects_path}${app_name}/logs:/var/logs -dit ${harbor_registry}/${image_prefix}/${app_name}:${version}
 echo 'run container '${app_name}' success!'
 
 sudo rm -rf ${projects_path}${app_name}
+
